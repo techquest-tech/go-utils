f8e3f513b6d46476bf0fff50cc2f46457d5e635f
armenpn <armenpn@gmail.com>
2020-11-07 18:54:52 +0800

    add cli

---
+++ cli/cli.go
@@ -0,0 +1,49 @@
+package cli
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+//BuildVersion version for the app
+var BuildVersion = "development"
+
+//GitCommit golang version.
+var GitCommit = "unknown"
+
+//Version print App version details.
+func Version() *cli.Command {
+	return &cli.Command{
+		Name:    "version",
+		Aliases: []string{"v"},
+		Action: func(c *cli.Context) error {
+			logrus.WithFields(logrus.Fields{
+				"BuildVersion": BuildVersion,
+				"GitCommit":    GitCommit,
+			}).Info("version:")
+			return nil
+		},
+	}
+}
+
+//Cleanup func be called when app close.
+type Cleanup func()
+
+//CloseOnlyNotified make app close only when notify
+func CloseOnlyNotified(c Cleanup) {
+	sigCh := make(chan os.Signal, 1)
+
+	signal.Notify(sigCh, os.Interrupt)
+	signal.Notify(sigCh, os.Kill)
+
+	select {
+	case <-sigCh:
+		logrus.Infof("Got Interrupt, app existing...")
+		if c != nil {
+			c()
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/sirupsen/logrus v1.7.0
+	github.com/urfave/cli v1.22.5
 	github.com/valyala/fasttemplate v1.2.1
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 )
